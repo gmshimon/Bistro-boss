826a27ae8962b7fdce284e52cc21f8d8497e47b1
gmshimon <simon.rosedale99@gmail.com>
2024-07-12 18:37:30 +0800

    frontend project initialized

---
+++ bistro-boss-client/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import { router } from './Routes/Routes';
+import {Provider} from 'react-redux'
+import store from './Redux/store';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>,
+)
