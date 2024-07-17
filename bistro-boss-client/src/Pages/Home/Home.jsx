f64a214ae5af93c18683ee2a3f534d9fa2452077
gmshimon <simon.rosedale99@gmail.com>
2024-07-17 17:05:07 +0800

    category slide added

--- bistro-boss-client/src/Pages/Home/Home.jsx
+++ bistro-boss-client/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
+import Category from '../../Components/Category/Category';
 
 const Home = () => {
     return (
         <div>
            <Banner/>
+           <Category/>
         </div>
     );
 };
