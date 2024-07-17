c28336cc47bd3a87e2b648dec35dceb3b94ac260
gmshimon <simon.rosedale99@gmail.com>
2024-07-17 08:58:34 +0800

    banner done

--- bistro-boss-client/src/Pages/Home/Home.jsx
+++ bistro-boss-client/src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Banner from '../../Components/Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-           
+           <Banner/>
         </div>
     );
 };
